e53097d710a07f70665543f0a00949b1ef809374
kubala.p <kubala.p@stu.janesville.k12.wi.us>
2022-09-07 10:36:49 -0500

    k

--- src/Prog52a.java
+++ src/Prog52a.java
@@ -3,12 +3,17 @@ public class Prog52a {
         int length = 143;
         int width = 82;
 
-        double area = 0;
-        double prim = 0;
+        int area = 0;
+        int prim = 0;
 
         area = length * width;
         prim = 2 * (length + width);
 
         System.out.println("The are of the rectangle is : " + area + "\n" + "The primeter of the : " + prim);
+
+        /*
+        Area = 11726
+        Perim = 450
+        */
     }
 }
