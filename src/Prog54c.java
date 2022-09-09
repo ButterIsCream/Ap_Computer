4a031cfec75699697dd8ccba96e2bd36103571cb
kubala.p <kubala.p@stu.janesville.k12.wi.us>
2022-09-09 10:22:04 -0500

    52a input

---
+++ src/Prog54c.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Prog54c {
+    public static void main(String[] strings) {
+        Scanner listner = new Scanner(System.in);
+
+        double rad = 0;
+
+        System.out.print("Give Radius : ");
+
+    }
+}
