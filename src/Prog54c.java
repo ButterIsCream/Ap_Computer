f49e97738e2c4da88e7bc23f3b9f7b32fc4742dd
kubala.p <kubala.p@stu.janesville.k12.wi.us>
2022-09-12 10:31:32 -0500

    52a input

--- src/Prog54c.java
+++ src/Prog54c.java
@@ -1,14 +1,33 @@
 import java.util.*;
 
 public class Prog54c {
+
+    public static double round(double number,double decimals) {
+        double rounded = 0;
+
+        rounded = Math.round(number / decimals) * decimals;
+
+        return rounded;
+    }
     public static void main(String[] strings) {
         Scanner listner = new Scanner(System.in);
 
         double pi = 3.14159;
         double rad = 0;
+        double cir = 0;
+        double area = 0;
 
+        System.out.print("Input the radius : ");
+        rad = listner.nextDouble();
 
-        System.out.print("Give Radius : ");
+        area = pi * Math.pow(rad,2);
+        cir = 2 * (rad * pi);
 
+        area = round(area,10);
+        cir = round(cir,10);
+
+        System.out.println("The radius is : " + rad);
+        System.out.println("The area is : " + area);
+        System.out.println("The circumfrience is : " + cir);
     }
 }
