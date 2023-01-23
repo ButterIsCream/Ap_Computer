863379b933e3c0e05c9830fc042b46bd8c08b64e
kubala.p <kubala.p@stu.janesville.k12.wi.us>
2023-01-23 10:16:47 -0600

    done 1/14/23

---
+++ src/Prog464b.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog464b {
+    public static int[][] transpose(int[][] mat) {
+        int[][] transposed = new int[mat[0].length][mat.length];
+
+        for(int r = 0; r < mat.length; r++) {
+            for(int c = 0; c < mat[0].length; c++) {
+                transposed[c][r] = mat[r][c];
+            }
+        }
+
+        return transposed;
+    }
+
+
+
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("./src/data/prog464b.dat"));
+
+            int[][] mat = new int[5][5];
+
+            for(int r = 0; r < mat.length; r++) {
+                for(int c = 0; c < mat[0].length; c++) {
+                    mat[r][c] = input.nextInt();
+                }
+            }
+
+            int[][] transposed = transpose(mat);
+
+            System.out.println("Orignal Matrix : ");
+
+            for(int[] r : mat) {
+                for(int c : r) {
+                    System.out.print(c + "");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Transposed Matrix : ");
+
+            for(int[] r : transposed) {
+                for(int c : r) {
+                    System.out.print(c + "");
+                }
+                System.out.println();
+            }
+;        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/dfile.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class dfile {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File(""));
+        } catch(IOException e) {
+
+        }
+    }
+}
