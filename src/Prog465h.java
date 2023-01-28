25f31a68366fc587ae6234243b79cd81f9e27fb1
peter <kubala.p@stu.janesville.k12.wi.us>
2023-01-27 19:54:51 -0600

    done 1/25/23

--- src/Prog465h.java
+++ src/Prog465h.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Prog477a {
+public class Prog465h {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("./src/data/prog465h.dat"));
