430ebeeffa90a269c164437113c654f5518452a5
peter <kubala.p@stu.janesville.k12.wi.us>
2023-03-05 21:10:22 -0600

    done 3/5/23

--- src/Prog607a.java
+++ src/Prog607a.java
@@ -21,6 +21,16 @@ public class Prog607a {
             } else if(option == 1) {
                 System.out.print("Enter date : ");
                 date = input.next();
+                String[] split = date.split("/");
+                String d1 = split[2].substring(0,1);
+                String d2 = split[2].substring(1,2);
+
+                char day = (char)(64 + Integer.parseInt(split[0]));
+                char month = (char)(67 + Integer.parseInt(split[1]));
+                char ed1 = (char)(76 + Integer.parseInt(d1));
+                char ed2 = (char)(75 + Integer.parseInt(d2));
+
+                System.out.println(day + "" + month + "" + "" + ed1 + "" + ed2);
             } else if(option == 2) {
 
             }
