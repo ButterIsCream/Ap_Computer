871fca0b013dab0501c8f3e57e9fa1fc0307d58c
peter <kubala.p@stu.janesville.k12.wi.us>
2023-03-26 17:48:06 -0500

    done 3/26/2023

--- src/Prog910b.java
+++ src/Prog910b.java
@@ -1,24 +1,30 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Prog910b {
-    public static String convert(int num,String b) {
-        b += num % 2;
-        num = num / 2;
+    public static String convert(int num,int base,String b) {
+        b += num % base;
+        num = num / base;
 
-        if(!(num / 2 == 0)) {
-            return convert(num,b);
+        if(!(num / base == 0)) {
+            return convert(num,base,b);
         }
 
         return b;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numConv = 0;
 
-        System.out.print("Enter the number to be converted : ");
-        numConv = input.nextInt();
+        int base10 = 0;
+        int newBase = 0;
 
-        String b = convert(numConv,"");
-        System.out.println(b);
+        System.out.print("Enter Number[Base 10] : ");
+        base10 = input.nextInt();
+        System.out.print("Enter New Base : ");
+        newBase = input.nextInt();
+        String e = convert(base10,newBase,"");
+
+        System.out.println(e);
     }
 }
